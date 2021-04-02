822f25fb9b8a548788db24111e4b8a0433315966
kkwon <kkwoncokr@gmail.com>
2021-04-02 16:54:55 +0900

    [Feat] users modules 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       debug: true,
       playground: true,
     }),
+    UsersModule,
   ],
 })
 export class AppModule {}
