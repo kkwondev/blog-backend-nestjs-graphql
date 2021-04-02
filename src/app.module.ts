0f16e92da8583aba74b09f1e2707f7310189165d
kkwon <kkwoncokr@gmail.com>
2021-04-02 16:52:07 +0900

    [Feat] 기본셋팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.MTSQL_USERNAME,
+      password: process.env.MTSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE
+        ? process.env.MTSQL_DATABSE
+        : 'kbloglocal',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      migrations: ['migration/*{.ts,.js}'],
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      debug: true,
+      playground: true,
+    }),
+  ],
 })
 export class AppModule {}
