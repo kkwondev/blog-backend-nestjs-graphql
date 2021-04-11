b4bf7ef4ce65842aa33ecfb1f34f68e5f36a3b09
kkwon <kkwoncokr@gmail.com>
2021-04-11 19:47:57 +0900

    [Fix] user entity에서 posts를 null값을 허용해라

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -1,27 +1,23 @@
-import { ObjectType } from '@nestjs/graphql';
+import { ObjectType, Field } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Post } from 'src/posts/entities/posts.entity';
-import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  OneToMany,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 
 @Entity('User')
 @ObjectType('User')
 export class User extends CoreEntity {
+  @Field((type) => String)
   @Column()
   email: string;
 
+  @Field((type) => String)
   @Column()
   password: string;
 
+  @Field((type) => String)
   @Column()
   nickname: string;
-
+  @Field((type) => [Post], { nullable: true })
   @OneToMany((type) => Post, (post) => post.user)
   posts: Post[];
 }
