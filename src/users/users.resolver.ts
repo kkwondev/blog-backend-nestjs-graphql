27a3d3b5cde458f1ed2a4dbfd37528a5f57ac9d1
kkwon <kkwoncokr@gmail.com>
2021-04-11 19:48:39 +0900

    [Fix] user Dto를 수정하라

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,26 +1,26 @@
 import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
-import { CreateUserDto } from './interfaces/create-user.dto';
+import { User } from './entities/users.entity';
 import { InputUser } from './interfaces/user.input';
 import { UsersService } from './users.service';
 @Resolver('User')
 export class UsersResolver {
   constructor(private readonly userService: UsersService) {}
 
-  @Query(() => [CreateUserDto])
+  @Query(() => [User])
   @UseGuards(AuthGuard)
   async getUsers() {
     return await this.userService.findAll();
   }
 
-  @Query(() => CreateUserDto)
+  @Query(() => User)
   @UseGuards(AuthGuard)
   async getUser(@Args('id') id: number) {
     return await this.userService.findOne(id);
   }
 
-  @Mutation(() => CreateUserDto)
+  @Mutation(() => User)
   @UseGuards(AuthGuard)
   async createUser(@Args('user') user: InputUser) {
     return await this.userService.createUser(user);
