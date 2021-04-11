991ef7f9d4cf8a7b4674ca0de59ed8af2f4927b0
kkwon <kkwoncokr@gmail.com>
2021-04-11 17:50:10 +0900

    [Refactor] entity 폴더로 이동

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,8 @@
 import { forwardRef, Module } from '@nestjs/common';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
 import { AuthService } from 'src/auth/auth.service';
-import { User } from './users.entity';
+import { User } from './entities/users.entity';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './interfaces/create-user.dto';
-import { User } from './users.entity';
+import { User } from './entities/users.entity';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
