4a81e7557a6853cd25ccc7c33aa52076f657f422
kkwon <kkwoncokr@gmail.com>
2021-04-26 00:09:16 +0900

    [Fix] aws s3 경로 추가와 사이즈 limit를 걸어라

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -50,14 +50,21 @@ export class ImageService {
     }
   }
   upload = multer({
+    fileFilter: function fileFilter(req, file, cb) {
+      if (file.mimetype.substring(0, 'image'.length) == 'image') {
+        cb(null, true);
+      } else {
+        cb(null, false);
+      }
+    },
     storage: multerS3({
       s3: s3,
       bucket: AWS_S3_BUCKET_NAME,
       acl: 'public-read',
-      mimetype: 'image/*',
       key: function (request, file, cb) {
-        cb(null, `${Date.now().toString()} - ${file.originalname}`);
+        cb(null, `uploads/${Date.now().toString()} - ${file.originalname}`);
       },
     }),
+    limits: { fileSize: 20 * 1024 * 1024 },
   }).array('upload', 1);
 }
