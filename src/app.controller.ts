e5c285f8e6336a6d1aba6d9ca73eb4ee47fef3fd
kkwondev <kkwoncokr@gmail.com>
2021-05-21 23:54:38 +0900

    get / return modify

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,8 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get('/')
   app() {
-    return 'Hello kkwon blog api';
+    return {
+      message: 'hello world',
+    };
   }
 }
